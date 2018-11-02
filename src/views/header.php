29ebb7ea86093fd4fc75c1a1e61e34578b9cf1b3
Stephen Ritchie <stephenfritchie@gmail.com>
2018-11-01 20:29:49 -0400

    Updated header/footer to work correctly

--- src/views/header.php
+++ src/views/header.php
@@ -1,93 +1,54 @@
-
-<!-- Loaded into every file - Contains all includes for every library used throughout the site -->
 <?php
-  //error_reporting(0);
+  /**
+   *
+   *
+   *
+   */
   session_start();
-  //require_once("../feature_connector.php");
 ?>
 
 
-
 <!DOCTYPE html>
-
 <html lang="en">
-
-
-
   <head>
-
+    <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SQS Training</title>
 
-    <meta name="description" content="">
-
-    <meta name="author" content="">
-
-
-
-    <!-- Bootstrap core CSS -->
-
-    <link href="../assets/css/bootstrap.css" rel="stylesheet">
-
-    <link href="../assets/css/main.css" rel="stylesheet">
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/jquery.min.js"><\/script>')</script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-
-
-    <script type="text/javascript">
-      $(document).ready(function(){
-
-        if($(window).width() > 767){
-          $('.navbar .dropdown').hover(function() {
-            $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-
-          }, function() {
-            $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
-
-          });
-
-          $('.navbar .dropdown > a').click(function(){
-            location.href = this.href;
-          });
+    <style>
+      body {
+        padding-top: 5rem;
         }
-      });
-    </script>
-
-
+        .starter-template {
+          padding: 3rem 1.5rem;
+          text-align: center;
+      }
+    </style>
   </head>
-
-
-
   <body>
 
-
-    <?php
-      //feature_loader("navigation", isset($_SESSION['user']) ? $_SESSION['user'] : -1);
-     ?>
-
-
-
-
-    <!-- Bootstrap core JavaScript
-
-    ================================================== -->
-
-    <!-- Placed at the end of the document so the pages load faster -->
-
-    <script src="../assets/js/bootstrap.min.js"></script>
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-
-    <!-- <script src="../assets/js/ie10-viewport-bug-workaround.js"></script>
-
-    -->
-  </body>
-
-</html>
-
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">SQS Training</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Groups</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SQS</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
