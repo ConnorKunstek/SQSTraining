c31a1e70f4ce634ab38ab4f470cab9e9d53735c4
Stephen Ritchie <stephenfritchie@gmail.com>
2018-11-01 21:47:20 -0400

    changed CSS stylesheet
    CSS style is now pulled from /assets instead of a CDN

--- src/views/header.php
+++ src/views/header.php
@@ -16,30 +16,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
 
     <title>SQS Training</title>
 
     <style>
       body {
-        padding-top: 5rem;
-        }
-        .starter-template {
-          padding: 3rem 1.5rem;
-          text-align: center;
+          padding-top: 5rem;
       }
     </style>
+
   </head>
   <body>
 
     <!-- Navbar -->
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#">SQS Training</a>
+
+      <a class="navbar-brand" href="#"><img src="/assets/img/logo.png" class="figure-img img-fluid rounded" width="45" height="45" alt="The SQS company logo."></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Profile</a>
           </li>
@@ -47,8 +48,12 @@
             <a class="nav-link" href="#">Groups</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">SQS</a>
+            <a class="nav-link" href="https://www.sqs.com/en/index.php" target="_blank">Corporate Site</a>
           </li>
         </ul>
+         <form class="form-inline my-2 my-lg-0">
+           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+         </form>
       </div>
     </nav>
