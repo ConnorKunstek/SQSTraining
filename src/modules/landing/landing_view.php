a6cd181cab61851c6ca46785ce543079f551f3a0
Stephen Ritchie <stephenfritchie@gmail.com>
2018-11-01 21:48:35 -0400

    added youtube and google map feature

--- src/modules/landing/landing_view.php
+++ src/modules/landing/landing_view.php
@@ -20,7 +20,47 @@ include("../../views/header.php"); # HTML header
 
   <button type="button" class="btn btn-primary" disabled>Start Testing</button>
 
-  <?php include $model->get_youtube_video(); ?>
+    <!-- YouTube Feature -->
+    <style>
+        .youtube_video {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-bottom: 56.25%;
+            margin-top: 5%;
+        }
+        .video {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+    <div id="youtube_video" class="youtube_video">
+        <iframe
+                class="video"
+                src="https://www.youtube.com/embed/UE3pkxPVLiw?autoplay=1&mute=1"
+                frameborder="0"
+                allow="autoplay; encrypted-media"
+                allowfullscreen>
+        </iframe>
+    </div>
+
+
+    <!-- Google Map Feature -->
+    <div id="googleMap" style="width:100%;height:400px;"></div>
+    <script>
+        function myMap() {
+            var mapProp= {
+                center:new google.maps.LatLng(51.508742,-0.120850),
+                zoom:5,
+            };
+            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+
 
 </div>
 
