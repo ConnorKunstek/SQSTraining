539dc58aefcd2e4fe3b5b50d022231b59a623159
Lucas Andrew <l_andrew26@Lukes-MacBook-Pro-4.local>
2018-11-10 02:48:57 -0500

    completed sign in

--- src/modules/sign_in/sign_in_model.php
+++ src/modules/sign_in/sign_in_model.php
@@ -1,7 +1,48 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: connor
+ * User: luke
  * Date: 10/24/18
  * Time: 7:48 PM
  */
+
+require_once ("../../lib/Connector.php");
+
+session_start();
+
+function verifyUserInfo($data) {
+
+    try {
+        $base = Connector::getDatabase();
+
+        $email = $data['email'];
+        $password = $data['password'];
+
+        $sql = "SELECT password FROM user WHERE email = '$email';";
+        $stmt = $base->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch();
+
+        if ($result['password'] == $password) {
+            $sql = "SELECT UID, first_name, last_name, verified FROM user WHERE email = '$email';";
+            $stmt = $base->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            return $result;
+            // write a verified check after implementation
+        } else {
+            errorModel('password');
+        }
+    } catch (Exception $e) {
+        throw ($e);
+    }
+}
+
+function errorModel($var) {
+    if ($var == 'password') {
+        $message = header("Invalid password! Please try again.");
+    }
+
+    return $message;
+}
+?>
