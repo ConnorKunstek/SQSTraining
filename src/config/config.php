5bece86eb4f4946991029af6479006b6c3de463d
Stephen Ritchie <stephenfritchie@gmail.com>
2018-11-30 17:55:36 -0500

    Configuration handling is now more robust.

--- src/config/config.php
+++ src/config/config.php
@@ -1,79 +1,56 @@
 <?php
 /**
+ * This script handles the bulk of retrieving values from configuration files.
  *
+ * @author Stephen Ritchie <stephen.ritchie@uky.edu>
+ * @author Connor Kunstek
  */
 
+require_once (__DIR__."/../../src/lib/ConfigurationInterface.php");
 
-// Loading configuration file.  If the configuration file cannot be opened an error page is displayed to the user and
-// the error is logged internally.
-$config_filename = __DIR__."/../../config/config.ini";
-if ($config_ini = parse_ini_file($config_filename, true)){
 
-    // Defining some constants from the config file.
-    define("VERSION", $config_ini['env']['version']);
-    define("ENV", $config_ini['env']['env']);
-    define("SENDMAIL", $config_ini['mail']['sendMail']);
-    define("DATABASE_FILENAME", $config_ini['database']['filename']);
-    define("LOG_LEVEL", $config_ini['logging']['minimumLevel']);
+// ---------------------------------------------------
+// Loading the system configuration file
+// ---------------------------------------------------
+// Get parsed config file from the ConfigurationInterface
+$config_ini = ConfigurationInterface::getInterface()->getSystemConfig();
 
-} else {
-    error_log("Could not open config file.");
-    header("Location: /src/views/error.php");
-    exit();
+// Define a few key constants using config files.  Also the entire config is stored in CONFIG as
+// an associative array.
+define("CONFIG", $config_ini);
+define("VERSION", $config_ini['env']['version']);
+define("ENV", $config_ini['env']['env']);
+define("SENDMAIL", $config_ini['mail']['sendMail']);
+define("DATABASE_FILENAME", $config_ini['database']['filename']);
+define("LOG_LEVEL", $config_ini['logging']['minimumLevel']);
+
+Logger::getInstance()->log_debug("system configuration loaded", basename(__FILE__));
+
+
+// ---------------------------------------------------
+// Loading the database configuration file
+// ---------------------------------------------------
+// Get parsed config file from the ConfigurationInterface
+$database_ini = ConfigurationInterface::getInterface()->getDatabaseConfig();
+
+// TODO: Remove this 'if' statement before initial release
+if (!file_exists(__DIR__.'/../../../'.$config_ini['database']['filename'])){
+    $database_ini = parse_ini_file("database.ini", true);
+    Logger::getInstance()->log_warning("Local database configuration being used...please create an .ini file outside root", basename(__FILE__));
 }
 
+// Define database credentials in CONSTANTS so that Connector can use them.
+$DB_SERVER = $database_ini[ENV]['hostname'];
+$DB_USER = $database_ini[ENV]['username'];
+$DB_PASSWORD = $database_ini[ENV]['password'];
+$DB_NAME = $database_ini[ENV]['name'];
+$DB_PORT = $database_ini[ENV]['port'];
 
-// Now that the config file has been parsed the Logger is instantiated, since parts of the Logger depend on values
-// being parsed from the config file.
-require_once (__DIR__."/../../src/lib/Logger.php");
-$logger = Logger::getInstance();
-$logger->log_debug("Configuration file successfully parsed.", basename(__FILE__));
-
-
-// Loading database credentials from separate file outside of root.  If the database credentials cannot be found an
-// error page is displayed to the user and the error is logged internally.
-// TODO: Remove hard-coded database values below.
-if ($ini = parse_ini_file(__DIR__.'/../../../'.DATABASE_FILENAME, true)){
-
-    $logger->log_debug("Successfully parsed database file.", basename(__FILE__));
-
-    if (ENV != "development" and ENV != "production"){
-        $logger->log_error("ENV is set to an invalid value=".ENV." Needs to be set to 'production' or 'development'");
-        header("Location: /src/views/error.php");
-        exit();
-    }
-
-    $logger->log_debug("Using a ".ENV." database.", basename(__FILE__));
-
-    $DB_SERVER = $ini[ENV]['hostname'];
-    $DB_USER = $ini[ENV]['username'];
-    $DB_PASSWORD = $ini[ENV]['password'];
-    $DB_NAME = $ini[ENV]['name'];
-    $DB_PORT = $ini[ENV]['port'];
-
-    // Checking to see if the database file has been configured.
-    if ($DB_SERVER == "XXX"){
-        $logger->log_error("Looks like the database .ini file has not been configured.", basename(__FILE__));
-        header("Location: /src/views/error.php");
-        exit();
-    }
-
-} else {
-
-    $logger->log_warning("Using hard-coded values for database credentials.", basename(__FILE__));
-
-    $DB_SERVER = '10.163.140.98';
-    $DB_USER = 'remote';
-    $DB_PASSWORD = 'password123';
-    $DB_NAME = 'SQSTrainingDB';
-    $DB_PORT = '3306';
-}
-
-
-// Defining database constants
 define('DB_SERVER', $DB_SERVER);
 define('DB_USER', $DB_USER);
 define('DB_PASSWORD', $DB_PASSWORD);
 define('DB_NAME', $DB_NAME);
 define('DB_PORT', $DB_PORT);
 define('DB_DSN', 'mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';port=' . DB_PORT);
+
+Logger::getInstance()->log_debug("database configuration loaded", basename(__FILE__));
