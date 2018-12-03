3025c4b45dd552d43722fe081f6facb5918a2a6e
Nick Sladic <nick.sladic@g.uky.edu>
2018-12-03 06:20:12 -0500

    added feature loader gui

--- src/modules/feature/feature_model.php
+++ src/modules/feature/feature_model.php
@@ -10,17 +10,39 @@ require_once ("../../lib/Connector.php");
 session_start();
 
 /**
- * @function:       getGroups
+ * @function:       getAllUsers
  * @params:         NA
  * @return          array|Exception
- * @Description:    retrieves all the current groups in the database
+ * @Description:    gets all users first name, last name, and email
  *
  */
-
 function getAllUsers(){
     try{
         $base = Connector::getDatabase();
         $sql = "SELECT user.UID, user.first_name, user.last_name, user.email FROM user;";
+        $stmt = $base->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        return $result;
+
+    }catch(Exception $e){
+        return $e;
+    }
+}
+
+
+/**
+ * @function:       getErrors
+ * @params:         $name
+ * @return          array|Exception
+ * @Description:    gets all $name error files
+ *
+ */
+function getErrors($name){
+    try{
+        $base = Connector::getDatabase();
+        $sql = "SELECT features_available.name, features_available.id FROM features_available WHERE features_available.feature_group_name = '$name'";
         $stmt = $base->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll();
@@ -31,3 +53,49 @@ function getAllUsers(){
     }
 }
 
+function getAssignedId($uid){
+    try{
+        $base = Connector::getDatabase();
+        $sql = "SELECT assigned_features.id FROM assigned_features WHERE assigned_features.user_id = '$uid'";
+        $stmt = $base->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }catch(Exception $e){
+        return $e;
+    }
+}
+
+function getAssignFeatures($uid){
+    try{
+        $base = Connector::getDatabase();
+        $sql = "SELECT features_available.id, features_available.name, features_available.feature_group_name
+                FROM features_available
+                JOIN assigned_features ON features_available.id = assigned_features.feature_number WHERE assigned_features.user_id = '$uid'";
+        $stmt = $base->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }catch(Exception $e){
+        return $e;
+    }
+}
+
+/**
+ * @function:       setFeatures()
+ * @params:         $uid - UID | $fid - id | $uid
+ * @return          array|Exception
+ * @Description:    sets the feature id assigned to the user
+ *
+ */
+function setFeatures($uid,$fid,$aid){
+    try{
+        $base = Connector::getDatabase();
+        $sql = "UPDATE assigned_features SET feature_number = '$fid' WHERE assigned_features.id = '$aid' AND assigned_features.uid = '$uid';";
+        $stmt = $base->prepare($sql);
+        $stmt->execute();
+
+    }catch(Exception $e){
+        return $e;
+    }
+}
